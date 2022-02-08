6b64f1779be1db00b38e1b5723589183834177f9
Dev Nolan <oplimpics@yahoo.com>
2022-02-07 19:32:13 -0800

    test commit for import purposes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { bgImage } from "./assets/cod_background.jfif";
+//import { useState } from 'react';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.backgroundImage} source={bgImage}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    height: 500,
+    width: 500,
+    justifyContent: "center",
+    alignContent: "center"
+  }
 });
